f29724baa2eefc08c01e9dfbc572bb66e44e3a5b
YAT?STATS <pcdaction@gmail.com>
2026-03-03 20:03:43 -0700

    Update schoolAssets.ts

--- src/lib/schoolAssets.ts
+++ src/lib/schoolAssets.ts
@@ -1,9 +1,13 @@
 // src/lib/schoolAssets.ts
 
-const S3_BASE = "https://yatstats-assets.s3.us-west-2.amazonaws.com"
+const S3_BASE = "https://yatstats-assets.s3.us-west-2.amazonaws.com";
+
+// use the S3 placeholder you showed
+const S3_SCHOOL_PLACEHOLDER = `${S3_BASE}/yatstats/ys_crest.svg`;
 
 export function getSchoolCrestUrl(schoolId?: number | string) {
-  if (!schoolId) return "/school-placeholder.png.png"
+  if (!schoolId) return S3_SCHOOL_PLACEHOLDER;
 
-  return `${S3_BASE}/schools/${schoolId}.png`
+  // if your school crests are PNGs by id, keep this:
+  return `${S3_BASE}/schools/${schoolId}.png`;
 }
