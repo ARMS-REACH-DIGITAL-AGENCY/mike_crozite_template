a263aec55693f6eaee9c690c3f855049a5507ce8
YAT?STATS <pcdaction@gmail.com>
2026-03-19 09:13:01 -0700

    Row 3 - InteractionStrip component

--- src/components/yatstats/shell/InteractionStrip.tsx
+++ src/components/yatstats/shell/InteractionStrip.tsx
@@ -1,7 +1,7 @@
 
 // src/components/yatstats/shell/InteractionStrip.tsx
 // Renders Row 3 of the shared shell.
-// This component is a placeholder and will be built out later.
+// This component is a placeholder and will be built out later
 
 interface InteractionStripProps {
   isPlayerProfile: boolean;
