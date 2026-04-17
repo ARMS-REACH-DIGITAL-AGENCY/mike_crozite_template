f25e924f77012b86a27c3ee4e90e69abb40bd1df
yatstats <227676268+yatstats@users.noreply.github.com>
2026-04-17 01:53:08 -0400

    Remove redundant ALUMNI HEADLINES title

--- src/app/[hsid]/page.tsx
+++ src/app/[hsid]/page.tsx
@@ -270,7 +270,6 @@ export default async function SchoolPage({
         <div className="yat-news-wrap">
           <div className="yat-news-header">
             <div>
-              <div className="yat-news-title">ACTIVE ALUMNI NEWS</div>
               <div className="yat-news-sub">Latest news mentions for {schoolName} baseball alumni</div>
             </div>
           </div>
