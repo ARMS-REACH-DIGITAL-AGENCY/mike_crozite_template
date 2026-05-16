14eb4371beb9d14ef130f1435a8be22a2eb64381
YAT?STATS <pcdaction@gmail.com>
2026-05-16 00:08:28 -0700

    Apply mobile viewport fix to player profiles

--- src/app/[hsid]/player/[playerId]/layout.tsx
+++ src/app/[hsid]/player/[playerId]/layout.tsx
@@ -8,6 +8,7 @@ import ProfileFunZoneStabilizer from '@/components/yatstats/ProfileFunZoneStabil
 import ProfileStatsInjector from '@/components/yatstats/ProfileStatsInjector';
 import ProfileFunZoneCleanupStyles from '@/components/yatstats/ProfileFunZoneCleanupStyles';
 import ProfileStatsFinalOverrides from '@/components/yatstats/ProfileStatsFinalOverrides';
+import ProfileMobileViewportFix from '@/components/yatstats/ProfileMobileViewportFix';
 import GoldenLineLogoDesignOverrides from '@/components/yatstats/GoldenLineLogoDesignOverrides';
 import CareerTimelineCtaOverrides from '@/components/yatstats/CareerTimelineCtaOverrides';
 
@@ -162,6 +163,7 @@ export default async function PlayerLayout({
       {children}
       <ProfileFunZoneCleanupStyles />
       <ProfileStatsFinalOverrides />
+      <ProfileMobileViewportFix />
       <GoldenLineLogoDesignOverrides />
       <CareerTimelineCtaOverrides />
     </PlayerProfileContextProvider>
