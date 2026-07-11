c4f123e9759d5c67bc4f49a1b2e63dd3157a1afe
YAT?STATS <pcdaction@gmail.com>
2026-07-10 22:46:42 -0700

    Remove unsupported interaction strip prop

--- src/components/yatstats/SharedShell.tsx
+++ src/components/yatstats/SharedShell.tsx
@@ -153,7 +153,6 @@ export default function SharedShell({
                     isGallery={isGallery}
                     isNews={isNews}
                     players={players}
-                    activeSection={activeSection}
                   />
                 )}
         </div>
